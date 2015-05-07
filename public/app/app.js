a653712d0a4eea8589515c27e1de6264bb112160
Slava <slavakvak@gmail.com>
2015-05-08 00:51:20 +0300

    add angular flexSlider

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,12 @@
 //libs
 window['jQuery'] = require('../vendor/jquery/dist/jquery.js');
+require('../vendor/flexslider/jquery.flexslider.js');
 require('../vendor/bootstrap/dist/js/bootstrap.js');
 require('../vendor/toastr/toastr.js');
 require('../vendor/angular/angular.js');
 require('../vendor/angular-resource/angular-resource.js');
 require('../vendor/angular-ui-router/release/angular-ui-router.js');
+require('../vendor/angular-flexslider/angular-flexslider.js');
 
 //controllers
 var mvMainCtrl = require('./main/mvMainCtrl');
@@ -14,7 +16,7 @@ var contactCtrl = require('./contact/contactCtrl');
 //services
 var mvNotifier = require('./common/mvNotifier');
 
-var appModule = angular.module('app', ['ngResource', 'ui.router'])
+var appModule = angular.module('app', ['ngResource', 'ui.router', 'angular-flexslider'])
 	.controller('mvMainCtrl', mvMainCtrl)
 	.value('mvToastr', toastr)
 	.factory('mvNotifier', mvNotifier);
--- public/app/main/mvMainCtrl.js
+++ public/app/main/mvMainCtrl.js
@@ -1,5 +1,15 @@
 var mvMainCtrl = function ($scope) {
-	$scope.helloWord = 'Hi from Angular'
+	$scope.mySlides = [
+		"../../img/IMG_9281.jpg",
+		"../../img/IMG_9282.jpg",
+		"../../img/IMG_9283.jpg",
+		"../../img/IMG_9284.jpg",
+		"../../img/IMG_9285.jpg",
+		"../../img/IMG_9286.jpg",
+		"../../img/IMG_9288.jpg",
+		"../../img/IMG_9289.jpg",
+		"../../img/IMG_9290.jpg"
+	];
 };
 
 mvMainCtrl.$inject = ['$scope'];
